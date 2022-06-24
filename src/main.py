1c433adb69c4121a496926800815f64480e33271
sunshanqian-kyushu-u <sunshanqian@outlook.com>
2022-06-24 22:06:24 +0900

    202206242206

--- src/main.py
+++ src/main.py
@@ -1,28 +1,116 @@
+import threading
+from time import sleep
 import cv2
 import datetime
+from queue import Queue
 
-from threading import Thread
-from gi.repository import GLib
+frame_read_flag = False
+frame_process_flag = False
+frame_finished_flag = False
+frame_get_flag = False
+link_flag = False
+link_result_flag = False
 
-# print(cv2.version)
+def frame_process(var):
+    global frame_finished_flag
+    global frame_process_flag
+    global frame_get_flag
+    
+    frame_finished_flag = True
+    sleep(1) # wait var get the first frame
+    print("frame process start!")
 
-main_loop = GLib.MainLoop()
-main_loop_thread = Thread(target=main_loop.run)
-main_loop_thread.start()
+    while frame_process_flag:
+        if frame_get_flag:
+            frame_get_flag = False
 
-video = cv2.VideoCapture('rtmp://192.168.10.126/live/password')
+            # test
+            print("start process.")
+            cv2.imwrite("build/image" + str(datetime.datetime.now()) + ".png", var.get())
+            sleep(5)
+            print("process finished.")
 
-try:
+            # image process
+
+
+            frame_finished_flag = True
+        else:
+            sleep(0.1)
+
+def frame_read(var):
+    global frame_process_flag
+    global frame_get_flag
+    global frame_read_flag
+    global frame_finished_flag
+    global link_flag
+    global link_result_flag
+
+    cap = cv2.VideoCapture("rtmp://192.168.10.126/live/password")
+    if(cap.isOpened()):
+        link_result_flag = True
+        link_flag = True
+        print("link to stream successfully!")
+
+        # start frame_process_thread
+        frame_process_thread = threading.Thread(target=frame_process, args=(var,))
+        frame_process_flag = True
+        frame_process_thread.start()
+
+        while frame_read_flag:
+            ret,frame = cap.read()
+
+            if frame_finished_flag:
+                frame_finished_flag = False
+                var.put(frame)
+                frame_get_flag = True
+
+        frame_process_flag = False
+        frame_process_thread.join()
+        cap.release()
+    else:
+        cap.release()
+        link_result_flag = False # as it defined
+        link_flag = True
+        print("can not link to the stream. please try again.")
+
+def main():
+    # print(threading.active_count())
+    # print(threading.enumerate())
+    # print(threading.current_thread())
+
+    var = Queue()
+    global frame_process_flag
+    global frame_get_flag
+    global frame_read_flag
+    global frame_finished_flag
+    global link_flag
+    global link_result_flag
+
+    frame_read_thread = threading.Thread(target=frame_read, args=(var,))
+    frame_read_flag = True
+    frame_read_thread.start()
+
     while True:
-        ret,frame = video.read()
-        if frame is None:
-            break
+        if not link_flag:
+            # do nothing
+            sleep(0.1)
+        else:
+            if link_result_flag:
+                # link succeed
+                break
+            else:
+                # link failed and exit directly
+                exit(1)
 
-        # save image
-        cv2.imwrite("build/image" + str(str(datetime.datetime.now())) + ".png", frame)
+    # wait for keyboard input to exit
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
 
-except KeyboardInterrupt:
-    pass
+    frame_read_flag = False
+    frame_read_thread.join() # wait frame_read_thread stopped
 
-main_loop.quit()
-main_loop_thread.join()
+if __name__ == '__main__':
+    main()
---
+++ src/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+L = np.zeros((3, 4))
+print(L[1, 2])
